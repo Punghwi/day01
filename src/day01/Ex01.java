344c4cef0050584eea218a01a6e031211aadb761
email <white487@naver.com>
2022-03-16 11:33:23 +0900

    Ex01.java

---
+++ src/day01/Ex01.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class Ex01 {
+	
+	public static void main(String[] arg) {
+		
+		System.out.println("안녕하세요!!!");
+		System.out.println("자바시간입니다");
+		
+		System.out.print("출력");
+		System.out.print("합니다");
+		//ctrl + alt + 방향키 테스트
+		//ctrl + alt + 방향키 테스트
+		//ctrl + alt + 방향키 테스트
+		//ctrl + alt + 방향키 테스트
+		//ctrl + alt + 방향키 테스트
+		//ctrl + alt + 방향키 테스트
+		//"" 는 문자열에 대한 의미를 지닌다.
+		
+		/*
+			여러줄 주석
+		*/
+	}
+}
